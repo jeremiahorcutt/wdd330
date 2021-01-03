858d17437942b9dfdfc5ac764279c01bf805d236
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-02 18:13:16 -0700

    a

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const links=[
         url: "week1/index.html"
     }
 ]
+
+for (let i = 0; i < links.length; i++ ) {
+  let list = document.createElement('li');
+  let link = document.createElement('a');
+  link.setAttribute("src", link[i].url);
+  link.textContene = link[i].label;
+};

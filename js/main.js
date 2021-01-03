b9ce0af53920afd626ec3488019d20c3fb9f611e
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-02 18:53:17 -0700

    a

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const links=[
 for (let i = 0; i < links.length; i++ ) {
   let list = document.createElement('li');
   let alink = document.createElement('a');
-  alink.setAttribute("src", links[i].url);
+  alink.setAttribute("href", links[i].url);
   alink.textContent = links[i].label;
   list.appendChild(alink);
   document.querySelector('ol#tableofcontents').appendChild(list);

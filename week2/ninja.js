f7c46d88a3b276250e0d15c3848c9c4ec62b6515
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-08 15:13:16 -0700

    a

--- week2/ninja.js
+++ week2/ninja.js
@@ -3,15 +3,29 @@ const quiz = [
     ["What is Wonder Woman's real name?","Diana Prince"],
     ["What is Batman's real name?","Bruce Wayne"]
 ];
-let score = 0 
-
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
+function start(quiz){
+    let score = 0;
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+    gameOver();
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+    function check(response,answer){
+        if(response === answer){
         alert('Correct!');
         score++;
-    } else {
+        } else {
         alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+start(quiz);

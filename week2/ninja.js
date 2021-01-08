5a917677b51a3746116a2018e3ac2bfcd04bba75
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-08 14:43:41 -0700

    a

--- week2/ninja.js
+++ week2/ninja.js
@@ -2,6 +2,5 @@ const question = "what is Superman's real name?"
 
 const answer = prompt(question);
 
-function display(){
-    alert('You answered ${answer}');
-};
+
+alert('You answered ${answer}');

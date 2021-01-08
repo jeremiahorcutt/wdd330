320541025ccebbed5438fb92093052854ce52719
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-08 16:55:16 -0700

    a

--- week2/projects.js
+++ week2/projects.js
@@ -8,6 +8,17 @@ console.log(loop)
 
 function fizzbuzz(){
     for(let j = 0; j < 101; j++){
-
+    if(j % 3 == 0){
+        console.log("Fizz")
+    }
+    else if(j % 5 == 0){
+        console.log("Buzz")
+    }
+    else if(j % 3 == 0 && j % 5 == 0){
+        console.log("FizzBuzz")
+    }
+    else{
+        console.log(j)
+    }
     }
 }

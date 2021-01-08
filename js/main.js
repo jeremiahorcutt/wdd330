d1f67dfa829cc14429b1eb80e16cd288cfe07dcd
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-08 14:28:27 -0700

    a

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const links=[
     {
         label: "Week1 Notes",
-        url: "week1/index.html",
+        url: "week1/index.html"
+    },{
         label: "Week2 Notes",
         url: "week2/index.html"
     }

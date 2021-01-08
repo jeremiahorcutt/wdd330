7891435c746f5630f10419232e5792849cd6b498
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-08 16:47:26 -0700

    a

--- week2/projects.js
+++ week2/projects.js
@@ -1,7 +1,12 @@
 function looping(){
 let loop = "#";
 for(let i = 0; i < 7; i++){
-loop = loop + "#";
-document.getElementById("loop" + [i]).innerHTML = loop;
+console.log(loop)
 }
+}
+
+function fizzbuzz(){
+    for(let j = 0; j < 101; j++){
+
+    }
 }

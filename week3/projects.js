73cfeffaa9df6aff10e6b3df77bb4c0cd9177ecd
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-13 18:05:20 -0700

    a

--- week3/projects.js
+++ week3/projects.js
@@ -44,3 +44,14 @@ function get_number2(){
      const results = fun(get_number1(), get_number2());
      console.log(results);
  }
+
+ //14.1 Build a Table
+ const MOUNTAINS = [
+    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+    {name: "Everest", height: 8848, place: "Nepal"},
+    {name: "Mount Fuji", height: 3776, place: "Japan"},
+    {name: "Vaalserberg", height: 323, place: "Netherlands"},
+    {name: "Denali", height: 6168, place: "United States"},
+    {name: "Popocatepetl", height: 5465, place: "Mexico"},
+    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+  ];

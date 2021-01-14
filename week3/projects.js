0c41d465cd4175a9715c20dd0185839335664609
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-13 17:23:55 -0700

    a

--- week3/projects.js
+++ week3/projects.js
@@ -16,3 +16,25 @@ let calculator = {
   alert( calculator.sum() );
   alert( calculator.mul() );
 }
+
+function get_number1(){
+   let a = parseFloat(document.getElementById("number_1").value);
+}
+function get_number2(){
+    let b = parseFloat(document.getElementById("number_2").value);
+ }
+ function add(a, b){
+     return a + b;
+ }
+ function sub(a,b){
+     return a - b;
+ }
+ function mul(a, b){
+     return a * b;
+ }
+ function calculator(fun){
+     fun(
+         get_number1(),
+         get_number2()
+     );
+ }

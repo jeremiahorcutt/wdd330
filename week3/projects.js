870af118295cb01a6bc2e4a74798848e368c5ed9
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-21 10:49:21 -0700

    a

--- week3/projects.js
+++ week3/projects.js
@@ -85,17 +85,26 @@ function get_number2(){
   print.appendChild(table);
  };
  
-window.addEventListener("keydown", event => {
-    if (event.key == "ArrowUp") {
-        let counter = document.getElementById('balloon').style.fontSize;
-        console.log(counter);
-        let newcounter = counter + (counter * .1);
-        document.getElementById('balloon').style.fontSize = newcounter + "px";
-    }
-});
-window.addEventListener("keydown", event => {
- if(event.key == "ArrowDown"){
-    let newcounter = counter - (counter * .1);
-    document.getElementById('balloon').style.fontSize = newcounter + "px";
-}
-});
+ let balloon = document.getElementById("balloon");
+ let size;
+ function setSize(newSize) {
+   size = newSize;
+   balloon.style.fontSize = size + "px";
+ }
+ setSize(20);
+
+ function handleArrow(event) {
+   if (event.key == "ArrowUp") {
+     if (size > 70) {
+       balloon.textContent = "💥";
+       document.body.removeEventListener("keydown", handleArrow);
+     } else {
+       setSize(size * 1.1);
+       event.preventDefault();
+     }
+   } else if (event.key == "ArrowDown") {
+     setSize(size * 0.9);
+     event.preventDefault();
+   }
+ }
+ document.body.addEventListener("keydown", handleArrow);

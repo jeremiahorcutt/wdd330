22e938b00e2df3dc3b9f6b650d58e2b410181d14
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-22 16:59:10 -0700

    a

---
+++ week4/projects.js
@@ -0,0 +1,56 @@
+//vector type
+class Vec{
+    constructor(x, y){
+        this.x = x;
+        this.y =y;
+    }
+    plus(other){
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    minus(other){
+        return new Vec(this.x - other.x, this.y- other.y);
+    }
+    get length(){
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
+
+//groups
+class Group {
+    constructor(){
+        this.groupies =[];
+    }
+    add(num){
+        if (!this.has(num)){
+        this.groupies.push(num);
+    }
+    }
+    delete(num){
+        this.groupies = this.groupies.filter(n => n !== num);
+    }
+    has(num){
+        this.groupies.includes(num);
+    }
+    static from(group){
+        let newgroup = new Group;
+        for (let num of group){
+            newgroup.add(num);
+        }
+        return newgroup;
+    }
+  }
+  
+  let group = Group.from([10, 20]);
+  console.log(group.has(10));
+  // → true
+  console.log(group.has(30));
+  // → false
+  group.add(10);
+  group.delete(10);
+  console.log(group.has(10));
+  // → false
+
+ 

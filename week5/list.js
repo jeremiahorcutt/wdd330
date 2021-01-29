955fd2b69d0b9cecaa08e22732ccc80fd6b47033
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-29 15:14:07 -0700

    a

--- week5/list.js
+++ week5/list.js
@@ -1,18 +1,20 @@
 (function(){
   
     var list = document.querySelector('#list'),
-        form = document.querySelector('form'),
+        all = document.querySelector('#all'),
+        active = document.querySelector('#active'),
+        completed = document.querySelector('#completed'),
+        add = document.querySelector('#add_btn'),
         item = document.querySelector('#item');
     
-    form.addEventListener('submit',function(e){
-      e.preventDefault();
-      list.innerHTML += '<li>' + item.value + '</li>';
+    add.addEventListener('click',function(e){
+      list.innerHTML += '<li><input type="checkbox" name="'item.value'" class="list_item"/>' + item.value + '<input type="button" name="remove" class="remove" value="X"</li>';
       save();
       item.value = "";
     },false)
     
-    list.addEventListener('click',function(e){
-      var t = e.target;
+    all.addEventListener('click',function(e){
+      let t = e.target;
       if(t.classList.contains('checked')){
         t.parentNode.removeChild(t);
       } else {
@@ -20,6 +22,26 @@
       }
       save();
     },false)
+
+    active.addEventListener('click',function(e){
+        let t = e.target;
+        if(t.classList.contains('checked')){
+          t.parentNode.removeChild(t);
+        } else {
+          t.classList.add('checked');
+        }
+        save();
+      },false)
+
+    completed.addEventListener('click',function(e){
+        let t = e.target;
+        if(t.classList.contains('checked')){
+          t.parentNode.removeChild(t);
+        } else {
+          t.classList.add('checked');
+        }
+        save();
+      },false)
     
     function save() {
       window.localStorage.myitems = list.innerHTML;

4cc87c569dfdd18baec9b229fd8b5fe2f6649018
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-03 19:44:30 -0700

    a

--- week5/Js/ToDos.js
+++ week5/Js/ToDos.js
@@ -109,6 +109,8 @@ function recomplete(){
   for(let i = 0; i < list.length; i++){
     if(list[i].completed == true){
       utilities.completeItem(list[i].id + "Content");
+      let btn = document.getElementById(list[i]);
+      btn.setAttribute('value', 'X');
     }
   }
 }

731b91f22ae152e5c17e7db06b66d927687e364b
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-04 08:38:55 -0700

    a

--- week5/Js/ToDos.js
+++ week5/Js/ToDos.js
@@ -123,9 +123,7 @@ function task(){
   for(let i = 0; i < list.length; i++){
     if(list[i].completed == false){
       counter ++;
-      console.log(counter)
     }
-    console.log("1 " + counter)
     document.getElementById('counter_span').innerHTML = counter;
   }
 }
@@ -152,7 +150,7 @@ function completebtn(e){
       ls.saveTodo(list[i]);
     }
   }
-  ls.loadTodos;
+  loadTodos;
   task();
 }
 

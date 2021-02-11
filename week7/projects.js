e6d7439535942f70e2c08d16dbcbe2c7bb25b6f3
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-10 19:32:41 -0700

    a

--- week7/projects.js
+++ week7/projects.js
@@ -1,9 +1,16 @@
-
+//Flatten
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
 function flatten(arrays){
 console.log(arrays.reduce((first, second) => first.concat(second), []));
-
-}
+};
 flatten(arrays);
-// → [1, 2, 3, 4, 5, 6]
+
+//Your Own Loop
+function loop(value, test, update, show){
+  for(let i = value; test(i); i = update(i)){
+     show(i);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);

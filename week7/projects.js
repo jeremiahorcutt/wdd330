c44e1bd900153b2170bfb4c743a1f22cb71d8ae8
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-11 13:30:57 -0700

    a

--- week7/projects.js
+++ week7/projects.js
@@ -30,3 +30,35 @@ console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
 // → true
+
+//Marvel Mashup
+
+
+
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    buildTable(this);
+    }
+  };
+  xhttp.open("GET", "https://jeremiahorcutt.github.io/wdd330/text.JSON", true);
+  xhttp.send();
+}
+
+function buildTable(heroes){
+  for(let i = 0; i < heroes.length; i++){
+    let tr = document.createElement('tr');
+    let alias = document.createElement("th");
+    let realName = document.createElement("td");
+    let bio = document.createElement("td");
+
+    alias.textContent = heroes[i].name;
+    realName.textConent = heroes[i].realName;
+    bio.textConent = heroes[i].bio;
+     tr.appendChild(alias, realName, bio);
+     let table = document.querySelector('#marvel');
+     table.appendChild(tr);
+  }
+}

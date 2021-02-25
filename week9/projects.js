a94f78058dd9747850ab939c576a3323d45edc3e
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-25 15:23:24 -0700

    a

---
+++ week9/projects.js
@@ -0,0 +1,13 @@
+var array = [1, 3, 5, 7, 11, 13, 17, 19]
+
+array.forEach(function(arrayItem){
+    let total = arrayItem * 10;
+    console.log(total);
+});
+
+array.forEach(twenty);
+
+function twenty(arrayItem){
+    let total = arrayItem * 20;
+    console.log("outcome 20: " + total);
+}

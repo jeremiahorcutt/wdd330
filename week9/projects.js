a6491f7035e75d38e65067310cfe05bdb5c885d9
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-02-26 10:49:27 -0700

    a

--- week9/projects.js
+++ week9/projects.js
@@ -50,3 +50,12 @@ setInterval( () => {
     angle = (angle + 2) % 360;
     squareElement.style.transform = `rotate(${angle}deg)`
 }, 1000/60);
+
+const squareElement2 = document.getElementById('square2');
+let angle2 = 0;
+function rotate() {
+    angle2 = (angle2 + 2)%360;
+    squareElement2.style.transform = `rotate(${angle2}deg)`
+    window.requestAnimationFrame(rotate);
+}
+const id = requestAnimationFrame(rotate);

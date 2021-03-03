cb0cc62ed9bc9d107af6d3fed1abc9c78333ab16
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-03 11:30:14 -0700

    a

--- week10/quakes/index.js
+++ week10/quakes/index.js
@@ -1,4 +1,4 @@
 import QuakesController from './quakesController.js';
 
-const myQuakesController = new QuakesController('#quakeList');
+const myQuakesController = new QuakesController('#quakeUl');
 myQuakesController.init();

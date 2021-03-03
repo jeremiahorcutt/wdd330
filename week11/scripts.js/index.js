c041eb75b97106c2be20c714fd0c01943659decf
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-03 15:49:37 -0700

    a

--- week11/scripts.js/index.js
+++ week11/scripts.js/index.js
@@ -14,7 +14,7 @@
     }
 } */
 
-fetch("https://periodic-table-of-elements.p.rapidapi.com/elements", {
+/* fetch("https://periodic-table-of-elements.p.rapidapi.com/elements", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "eb6e5ab689mshc4df250f16a2797p141cc9jsnf79302459340",
@@ -27,3 +27,20 @@ fetch("https://periodic-table-of-elements.p.rapidapi.com/elements", {
 .catch(err => {
 	console.error(err);
 });
+ */
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://periodic-table-of-elements.p.rapidapi.com/elements");
+xhr.setRequestHeader("x-rapidapi-key", "eb6e5ab689mshc4df250f16a2797p141cc9jsnf79302459340");
+xhr.setRequestHeader("x-rapidapi-host", "periodic-table-of-elements.p.rapidapi.com");
+
+xhr.send(data);

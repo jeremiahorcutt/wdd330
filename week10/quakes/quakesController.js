bc6ca09dd89da72976fa6ba3a49ccc1f61801304
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-03 10:32:48 -0700

    a

--- week10/quakes/quakesController.js
+++ week10/quakes/quakesController.js
@@ -1,7 +1,7 @@
   
 import { getLocation } from './utilities.js';
 import Quake from './quake.js';
-import QuakesView from '.quakesView.js';
+import QuakesView from './quakesView.js';
 
 // Quake controller
 export default class QuakesController {

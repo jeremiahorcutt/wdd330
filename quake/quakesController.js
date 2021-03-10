db46c0602742394c9abf632e46a525eab8df3676
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-10 14:41:18 -0700

    a

--- quake/quakesController.js
+++ quake/quakesController.js
@@ -39,7 +39,7 @@ export default class QuakesController {
     }
   }
 
-/*    async getQuakesByRadius(radius = 100) {
+    async getQuakesByRadius(radius = 100) {
     // this method provides the glue between the model and view. Notice it first goes out and requests the appropriate data from the model, then it passes it to the view to be rendered.
     //set loading message
     this.parentElement.innerHTML = 'Loading...';
@@ -58,5 +58,5 @@ export default class QuakesController {
   async getQuakeDetails(quakeId) {
     // get the details for the quakeId provided from the model, then send them to the view to be displayed
    
-  } */
+  } 
 } 

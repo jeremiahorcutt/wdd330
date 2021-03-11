0ddedd8a7c7b2f27ae48abcdd38f32c1a42f341a
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-11 14:33:38 -0700

    a

--- week11/scripts.js/index.js
+++ week11/scripts.js/index.js
@@ -2,40 +2,62 @@
 
 window.onload = function() {
     createTable();
+    groupRows();
   };
 
 
  function createTable(){
-    for(let i = 0; i < 126; i++){
-        let number = i + 1;
+    for(let i = 1; i < 127; i++){
         let li = document.createElement("li");
-
-        if(number < 2){
-            li.setAttribute("data-id", number);
-            li.setAttribute("id", "element" + number);
-        }else if (number >= 18 && number <= 20){
-            let numberMinus = number - 16;
-            li.setAttribute("data-id", numberMinus);
-            li.setAttribute("id", "element" + numberMinus);
-        }else if (number >= 31 && number <= 38){
-            let numberMinus2 = number - 26;
-            li.setAttribute("data-id", numberMinus2);
-            li.setAttribute("id", "element" + numberMinus2);
-        }else if (number >= 49 && number <= 93){
-            let numberMinus3 = number - 36;
-            li.setAttribute("data-id", numberMinus3);
-            li.setAttribute("id", "element" + numberMinus3);
-        }else if (number >= 94 && number <= 111){
-            let numberMinus4 = number - 22;
-            li.setAttribute("data-id", numberMinus4);
-            li.setAttribute("id", "element" + numberMinus4);
-        }else if(number >= 112 && number <= 126){
-            let numberMinus5 = number - 8;
-            li.setAttribute("data-id", numberMinus5);
-            li.setAttribute("id", "element" + numberMinus5);
+        if(i < 2){
+            li.setAttribute("data-id", i);
+            li.setAttribute("id", "element" + i);
+        }else if (i >= 18 && i <= 20){
+            let iMinus = i - 16;
+            li.setAttribute("data-id", iMinus);
+            li.setAttribute("id", "element" + i);
+        }else if (i >= 31 && i <= 38){
+            let iMinus2 = i - 26;
+            li.setAttribute("data-id", iMinus2);
+            li.setAttribute("id", "element" + i);
+        }else if (i >= 49 && i <= 93){
+            let iMinus3 = i - 36;
+            li.setAttribute("data-id", iMinus3);
+            li.setAttribute("id", "element" + i);
+        }else if (i >= 94 && i <= 111){
+            let iMinus4 = i - 22;
+            li.setAttribute("data-id", iMinus4);
+            li.setAttribute("id", "element" + i);
+        }else if(i >= 112 && i <= 126){
+            let iMinus5 = i - 8;
+            li.setAttribute("data-id", iMinus5);
+            li.setAttribute("id", "element" + i);
         }else{
             li.setAttribute("class", "emptBlock");
-        }; 
+            li.setAttribute("id", "element" + i);
+        };
+        let ul = document.querySelector("#periodicGrid");
+        ul.appendChild(li); 
     }
 } 
 
+function groupRows(){
+  for(let i = 1; i < 127; i++){
+    let element = document.getElementById('element'+ i); 
+    if(i <= 18){
+         element.setAttribute("class", "row1");
+      }else if(i >= 19 && i <= 36){
+        element.setAttribute("class", "row2");
+      }else if(i >= 37 && i <= 54){
+        element.setAttribute("class", "row3");
+      }else if(i >= 55 && i <= 72){ 
+        element.setAttribute("class", "row4");
+      }else if(i >= 73 && i <= 90){
+        element.setAttribute("class", "row5");
+      }else if(i >= 91 && i <= 108){
+        element.setAttribute("class", "row6");
+      }else if(i >= 109 && i <= 126){
+        element.setAttribute("class", "row7");
+      }
+}
+} 
